78f00871cc533443e27cb07c9add38f406a923f2
Evgeny Grushko <evgeny.grushko@gmail.com>
2016-12-29 14:00:33 +0200

    Test multithread implementation

--- src/main/java/QualificationRoundTaskOne.java
+++ src/main/java/QualificationRoundTaskOne.java
@@ -3,6 +3,9 @@ package main.java;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * **********************************************************
@@ -12,19 +15,38 @@ import java.util.List;
  */
 public class QualificationRoundTaskOne {
 
+    static SomeThing someThing;
+
+    static ExecutorService executor = Executors.newFixedThreadPool(5);
+
     public static void main(String[] args) throws IOException {
 
-        long startTime = System.currentTimeMillis();
-        List<Integer> listOfNumbers = FileOperator.getInputNumbersFromFile(System.getProperty("user.dir") + "/input.txt");
-        List<Integer> listOfResults = new ArrayList<>();
-        for (int i = 1; i <= listOfNumbers.get(0); i++) {
-            listOfResults.add(MathCalculation.calculateWay(listOfNumbers.get(i)));
-        }
-        FileOperator.writeFile(System.getProperty("user.dir") + "/output.txt", listOfResults);
-        long endTime = System.currentTimeMillis();
-        System.out.println("Execution time of program in seconds: " + ExecutionTimer.measureSecBetweenTimestamps(startTime, endTime));
-    }
 
+        long startTime = System.currentTimeMillis();
+
+
+        List<Integer> listOfResults = new ArrayList<>();
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                FileOperator.writeFile(System.getProperty("user.dir") + "/output.txt", listOfResults);
+                long endTime = System.currentTimeMillis();
+                System.out.println("Execution time of program in seconds: " + ExecutionTimer.measureSecBetweenTimestamps(startTime, endTime));
+            }
+        });
+
+        List<Integer> listOfNumbers = FileOperator.getInputNumbersFromFile(System.getProperty("user.dir") + "/input.txt");
+        for (int i = 1; i <= listOfNumbers.get(0); i++) {
+            final Integer foo = listOfNumbers.get(i);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    listOfResults.add(MathCalculation.calculateWay(foo));
+                }
+            }).start();
+
+        }
+
+    }
 
 }
 
---
+++ src/main/java/SomeThing.java
@@ -0,0 +1,17 @@
+package main.java;
+
+/**
+ * **********************************************************
+ * Created by Evgeny Grushko on 28.12.2016
+ * ***********************************************************
+ **/
+public class SomeThing implements Runnable {
+
+
+    public void run()
+    {
+        System.out.println("Additional thread started...");
+
+    }
+
+}
